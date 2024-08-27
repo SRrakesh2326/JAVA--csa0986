7a8d2c7c53195292ce3d44db07198e3fc274a9b9
rakesh <161273020+SRrakesh2326@users.noreply.github.com>
2024-08-27 09:17:00 +0530

    Add files via upload

---
+++ Onearray.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+class Twoarray{
+  public static void main(String[] args) {
+	  int a[][]=new int[10][10];
+	  int b[][]=new int[10][10];
+	  int c[][]=new int[10][10];
+	  int m,n,i,j;
+	  Scanner s = new Scanner(System.in);
+	 
+	  System.out.println("Enter no. of rows and columns: ");
+	  m = s.nextInt();
+	  n = s.nextInt();
+	  System.out.println("Enter A matrix values: ");
+	  for(i=1;i<=m;i++){
+		  for(j=1;j<=n;j++){
+			  a[i][j] = s.nextInt();
+		  }
+	  }
+	  System.out.println("Enter B matrix values: ");
+	  for(i=1;i<=m;i++){
+		  for(j=1;j<=n;j++){
+			  b[i][j] = s.nextInt();
+		  }
+	  }
+	  for(i=1;i<=m;i++){
+		  for(j=1;j<=n;j++){
+			  c[i][j]=a[i][j]+b[i][j];
+		  }
+	  }
+	  System.out.println("Sum of the matrix:");
+	  for(i=1;i<=m;i++){
+		  for(j=1;j<=n;j++)
+		  {
+			  
+			  System.out.print("\t"+c[i][j]);
+		  }
+		  System.out.print("\n");
+		  
+	  }
+
+	  
+
+
+
+  }
+}
+		
+		
+		
+        
+    
+	
+ 
